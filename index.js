e7a83ce402c90752e3f9fa6d52378b3865f0903c
Njmilon <njmilon1@gmail.com>
2022-11-08 23:07:56 +0600

    setup serversite and load service api added

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT||5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//db connect code
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aoukuaq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const servicesCollection = client.db('wanderlustDB').collection('services');
+        app.get('/services', async(req,res) =>{
+            const query = {}
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.limit(3).toArray();
+        res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.error(err))
+
+
+app.get('/', (req, res) => {
+  res.send('wanderlust server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
