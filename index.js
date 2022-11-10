606188739e723f1867eaf780e22c249d71a8190c
Njmilon <njmilon1@gmail.com>
2022-11-10 21:42:12 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -16,11 +17,35 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+      return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden access' });
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
+
 async function run() {
   try {
     const servicesCollection = client.db('wanderlustDB').collection('services');
     const reviewsCollection = client.db('wanderlustDB').collection('reviews')
 
+    //JWT token api
+    app.post('/jwt',(req,res) =>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'});
+      res.send({token});
+    })
     // homepage service api with only 3 services
     app.get('/services', async (req, res) => {
       const query = {}
